a74f733073eb8e04023fc17adff927047232f243
ADMIN <lyeslehara88@gmail.com>
2021-05-13 09:50:40 +0200

    modification db

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongose = require('mongoose');
 mongose
-    .connect('mongodb+srv://'+ process.env.DB_USER_PASS+'@finales.xzbm4.mongodb.net/myProjet',
+    .connect('mongodb://127.0.0.1:27017/CRM',
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
